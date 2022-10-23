b2207511886000888da1cc07c0d7ced9dcf591ba
jemiway <jemiway0816@gmail.com>
2022-10-23 11:56:11 +0800

    Initial Commit

---
+++ FoodBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodBook
+//
+//  Created by CHUN-CHIEH LU on 2022/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
