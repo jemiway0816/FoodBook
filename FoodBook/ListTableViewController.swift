42ab9e2120b5d0e2ff4e8db2a505e859e42ae421
jemiway <jemiway0816@gmail.com>
2022-10-30 15:54:12 +0800

    加入 searchBar

--- FoodBook/ListTableViewController.swift
+++ FoodBook/ListTableViewController.swift
@@ -9,26 +9,25 @@ import UIKit
 import Kingfisher
 import SQLite3
 
-
-
 //class ListTableViewController: UITableViewController ,UISearchResultsUpdating {
-    
-    class ListTableViewController: UITableViewController {
-    
 
+class ListTableViewController: UITableViewController {
+    
     private var db:OpaquePointer?
     
     var restRow = Restaurant(id: "C3_315080500H_000013", name: "望海巴耐餐廳/咖啡", description: "非常有特色的原住民餐點餐廳，位於台十一線8K區段上，是東海岸行經花蓮大橋進入東海岸國家風景區之後，花蓮遊客中心前，第一家餐飲服務業者；業者於建物外部以當地竹子搭蓋起大門及挑高竹亭，呈顯其自然風格建築形式是其特色。望海巴耐野菜餐廳位於台十一線8K區段上，是東海岸行經花蓮大橋進入東海岸國家風景區之後，花蓮遊客中心前，第一家餐飲服務業者；業者於建物外部以當地竹子搭蓋起大門及挑高竹亭，呈顯其自然風格建築形式是其特色。", add: "花蓮縣974壽豐鄉鹽寮村大橋22號", zipcode: 974, region: "花蓮縣", town: "壽豐鄉", tel: "886-9-37533483", openTime: "11:30 - 20:00", website: "", picture1: "https://www.eastcoast-nsa.gov.tw/image/41530/640x480", picDescribe1: "花蓮無敵海景咖啡餐廳-望海巴耐", picture2: "", picDescribe2: "", picture3: "", picDescribe3: "", px: 121.606110, py: 23.918950, classLevel: 9, map: "", parkingInfo: "")
     
     var restaurants = [Restaurant]()
     
-    var restaurants2 = [
-    
+    var searchResult = [
+        
         Restaurant(id: "C3_315080500H_000013", name: "望海巴耐餐廳/咖啡", description: "非常有特色的原住民餐點餐廳，位於台十一線8K區段上，是東海岸行經花蓮大橋進入東海岸國家風景區之後，花蓮遊客中心前，第一家餐飲服務業者；業者於建物外部以當地竹子搭蓋起大門及挑高竹亭，呈顯其自然風格建築形式是其特色。望海巴耐野菜餐廳位於台十一線8K區段上，是東海岸行經花蓮大橋進入東海岸國家風景區之後，花蓮遊客中心前，第一家餐飲服務業者；業者於建物外部以當地竹子搭蓋起大門及挑高竹亭，呈顯其自然風格建築形式是其特色。", add: "花蓮縣974壽豐鄉鹽寮村大橋22號", zipcode: 974, region: "花蓮縣", town: "壽豐鄉", tel: "886-9-37533483", openTime: "11:30 - 20:00", website: "", picture1: "https://www.eastcoast-nsa.gov.tw/image/41530/640x480", picDescribe1: "花蓮無敵海景咖啡餐廳-望海巴耐", picture2: "", picDescribe2: "", picture3: "", picDescribe3: "", px: 121.606110, py: 23.918950, classLevel: 9, map: "", parkingInfo: ""),
         Restaurant(id: "C3_315080500H_000016", name: "噶瑪蘭海產店", description: "沒有固定菜單，菜單依照時節隨時更新，主要客群來自附近居民及固定客戶，業者也和附近民宿配合，接待海天、118、沙漠風情、來去海邊等民宿的住房遊客。主要菜色為海鮮料理及噶瑪蘭風味料理，餐廳本身還有供簡易之餐飲服務，可容納約90~120人，這是一間原住民風格濃厚的餐廳。", add: "花蓮縣977豐濱鄉新社村42號", zipcode: 977, region: "花蓮縣", town: "豐濱鄉", tel: "886-3-8711339", openTime: "11:00 - 14:00、17:00 - 19:00", website: "", picture1: "https://www.eastcoast-nsa.gov.tw/image/51996/640x480", picDescribe1: "噶瑪蘭海產店", picture2: "", picDescribe2: "", picture3: "", picDescribe3: "", px: 121.537790, py: 23.653180, classLevel: 9, map: "", parkingInfo: ""),
         Restaurant(id: "C3_315080500H_000018", name: "口福海鮮餐廳", description: "位處石梯港邊，臨近昕陽餐廳，同時也是東部賞鯨一號的報名處，餐廳主要提供海鮮熱炒料理，室內空間可容納200人以上，淡季客源除了過路觀光客，主要接待當地及附近居民，旺季則以接待遊覽車團體客人為主，是吃飯加賞鯨的好所在。東海岸的石梯坪有湛藍的海景、豐富多樣的海產，口福海鮮餐廳就設在花蓮南區最大的石梯港旁，出產多種迴游魚類、底棲珊瑚礁魚類、東海岸龍蝦，每天早上自家的漁船出海捕撈最新鮮的海產，招牌餐點有生魚片、多種鮮魚料理、東海岸活龍蝦、新鮮鮑魚及各種山、海產料理，不僅品質極佳價錢更是經濟實惠，保證讓您大飽口福，是您旅行東海岸最佳的用餐休息站。", add: "花蓮縣977豐濱鄉石梯港82號", zipcode: 977, region: "花蓮縣", town: "豐濱鄉", tel: "886-3-8781041", openTime: "11:00 - 14:00 、 17:00 - 19:30", website: "", picture1: "https://www.eastcoast-nsa.gov.tw/image/41544/640x480", picDescribe1: "口福海鮮餐廳", picture2: "", picDescribe2: "", picture3: "", picDescribe3: "", px: 121.510850, py: 23.488160, classLevel: 9, map: "", parkingInfo: "")
-    
+        
     ]
+    
+    @IBOutlet weak var regionTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,45 +36,87 @@ import SQLite3
         db = (UIApplication.shared.delegate as? AppDelegate)?.getDB()
         
         //準備離線資料集
-        DispatchQueue(label: "data").sync {
-            self.getDataFromTable()
-            self.tableView.reloadData()
+        DispatchQueue(label: "data").async {
+           
+            self.getAllRest()
         }
-
-    
         
-        setRestData()
+        setMapRestData(restData: searchResult)
+        
+        //初始化下拉更新元件
+        let refreshControl = UIRefreshControl()
+        //下拉更新元件綁定valueChanged事件觸發handleRefresh函式
+        refreshControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        //將下拉更新元件設定給tableView
+        self.tableView.refreshControl = refreshControl
         
     }
-
-    func setRestData() {
-        print("setRest")
+    
+    //由下拉更新元件所觸發的事件
+    @objc func handleRefresh()
+    {
+        //設定下拉更新元件的文字
+        self.tableView.refreshControl?.attributedTitle = NSAttributedString(string: "更新中...")
+        //從資料庫讀取資料（to do）
+        //更新表格資料
+        self.tableView.reloadData()
+        
+        //資料更新完成後將表格恢復原位置
+        self.tableView.refreshControl?.endRefreshing()
+    }
+    
+    @IBAction func regionTextField(_ sender: Any) {
+        
+        print("regionTextField did end")
+        getSearchRest(searchStr: "")
+    }
+    
+    func setMapRestData(restData:[Restaurant]) {
+        
         let navController = tabBarController?.viewControllers?[1] as? UINavigationController
         let mapViewController = navController?.viewControllers.first as? MapViewController
         
-        print("restaurants = \(restaurants.count)")
-        mapViewController?.restData = restaurants
+        print("restaurants count = \(restData.count)")
+        mapViewController?.restData = restData
     }
-    
-    
-    
-//    func updateSearchResults(for searchController: UISearchController) {
-//        <#code#>
-//    }
     
     
     //MARK: - 自定函式
     
     
-    
-    //查詢資料庫，存放到離線資料集
-    func getDataFromTable()
-    {
+    func getAllRest() {
+        
         //先清空離線資料集
         restaurants.removeAll()
         
         //準備查詢用的sql指令
-        let sql = "select name,description,\"add\",region,town,picture1,picDescribe1,px,py from restaurant"
+        let sqlStr = "select name,description,\"add\",region,town,picture1,picDescribe1,px,py from restaurant"
+        
+        restaurants = getDataFromTable(sql: sqlStr)
+
+    }
+    
+    func getSearchRest(searchStr:String) {
+        
+        searchResult.removeAll()
+        
+        let regionStr = regionTextField.text ?? "新北市"
+        
+        //準備查詢用的sql指令
+        let sqlStr = "select name,description,\"add\",region,town,picture1,picDescribe1,px,py from restaurant where region='\(regionStr)'"
+        
+        searchResult = getDataFromTable(sql: sqlStr)
+        setMapRestData(restData: searchResult)
+        
+        self.tableView.reloadData()
+        
+    }
+    
+    //查詢資料庫
+    func getDataFromTable(sql:String) -> [Restaurant]
+    {
+        
+        var restAll = [Restaurant]()
         
         //將SQL指令轉換成C語言的字元陣列
         let cSql = sql.cString(using: .utf8)!
@@ -92,7 +133,7 @@ import SQLite3
             while sqlite3_step(statement!) == SQLITE_ROW
             {
                 //讀取當筆資料的每一欄
-
+                
                 if let name = sqlite3_column_text(statement!, 0) {
                     let strName = String(cString: name)
                     restRow.name = strName
@@ -102,7 +143,7 @@ import SQLite3
                     let strDescription = String(cString: description)
                     restRow.description = strDescription
                 }
-
+                
                 if let add = sqlite3_column_text(statement!, 2) {
                     let strAdd = String(cString: add)
                     restRow.add = strAdd
@@ -112,13 +153,13 @@ import SQLite3
                     let strRegion = String(cString: region)
                     restRow.region = strRegion
                 }
-
+                
                 
                 if let town = sqlite3_column_text(statement!, 4) {
                     let strTown = String(cString: town)
                     restRow.town = strTown
                 }
-
+                
                 
                 if let picture1 = sqlite3_column_text(statement!, 5) {
                     let strPicture1 = String(cString: picture1)
@@ -129,17 +170,17 @@ import SQLite3
                     let strPicDescribe1 = String(cString: picDescribe1)
                     restRow.picDescribe1 = strPicDescribe1
                 }
-                     
+                
                 let px = sqlite3_column_double(statement!, 7)
                 restRow.px = px
-       
+                
                 let py = sqlite3_column_double(statement!, 8)
                 restRow.py = py
                 
-                print(restRow)
+                //                print(restRow)
                 
                 //將整筆資料加入陣列（離線資料集）
-                restaurants.append(restRow)
+                restAll.append(restRow)
             }
             //關閉連線資料集
             if statement != nil
@@ -152,23 +193,24 @@ import SQLite3
             print("資料庫查詢指令執行失敗")
         }
         
+        return restAll
     }
     
     
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return restaurants.count
+        return searchResult.count
     }
-
+    
     @IBSegueAction func showDetail(_ coder: NSCoder) -> DetailTableViewController? {
         
         let controller = DetailTableViewController(coder: coder)
         
         if let row = tableView.indexPathForSelectedRow?.row {
             
-            controller?.rest = restaurants[row]
+            controller?.rest = searchResult[row]
             
         }
         return controller
@@ -177,59 +219,29 @@ import SQLite3
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "\(ListTableViewCell.self)", for: indexPath) as! ListTableViewCell
         
-        let rest = restaurants[indexPath.row]
+        let rest = searchResult[indexPath.row]
         
         cell.cellImageView.kf.setImage(with: URL(string: rest.picture1))
         cell.cellNameLabel.text = rest.name
         cell.cellRegionLabel.text = "\(rest.region) \(rest.town)"
-
+        
         return cell
     }
+
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    func fetchSearch(name:String) {
+        getSearchRest(searchStr: name)
     }
-    */
+    
+}
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+
+extension ListTableViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        print("SearchButtonClicked did end")
+        fetchSearch(name: searchBar.text ?? "")
+        view.endEditing(true)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- FoodBook/MapViewController.swift
+++ FoodBook/MapViewController.swift
@@ -7,20 +7,17 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     
     var mLocationManager :CLLocationManager!
     var mMapView :MKMapView!
-    
-    
-    var test = 123
-    
+
     var restData:[Restaurant]!
     
     var myLocate = [
-        CLLocation(latitude: 25.034012, longitude: 121.56446),
         CLLocation(latitude: 25.034012, longitude: 121.56446)
     ]
     
@@ -28,25 +25,13 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        if let restData {
-//            print("count = \(restData.count)")
-//        } else {
-//            print("no data")
-//        }
-//        print("restaurant = \(restaurants.count)")
-        
-        print("map", test)
-        
-        showMap()
-        
     }
 
     override func viewWillAppear(_ animated: Bool) {
         
+        print("restData.count = \(restData.count)")
+        showMap()
     }
-    
-    
-
     
     func showMap() {
         
@@ -64,61 +49,57 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         // 取得自身定位位置的精確度
         mLocationManager.desiredAccuracy = kCLLocationAccuracyBest
         
-        //授權同意後取得使用者位置後指派給hereForNow
-        if let _ = mLocationManager?.location?.coordinate {
+        let searchLocate = CLLocation(latitude: restData[0].py, longitude: restData[0].px)
+        
+        //授權同意後取得使用者位置後指派給 hereForNow
+        if let hereForNow = mLocationManager?.location?.coordinate {
+            
+            print("hereForNow => \(hereForNow)")   // hereForNow => CLLocationCoordinate2D
             
             if let myCL = mLocationManager.location {
                 
                 myLocate.insert(myCL, at: 0)
+                myLocate.insert(searchLocate, at: 0)
                 
                 locationManager(mLocationManager, didUpdateLocations: myLocate)
                 
-                print("------ ok ")
-            } else {
-                print("------ ng ")
             }
         } else {
-            print("no no no ")
+            print("can't get hereForNow")
         }
-
     }
     
-  
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
+
         //取得當下座標
         let currentLocation :CLLocation = locations[0] as CLLocation
+
+        print("now locate = \(currentLocation)")
         
         //總縮放範圍
         let range:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-     
-        //自身
+
+        //顯示地圖
         let myLocation = currentLocation.coordinate
         let appearRegion:MKCoordinateRegion = MKCoordinateRegion(center: myLocation, span: range)
 
-
-//        objectAnnotation.coordinate = CLLocationCoordinate2D(latitude: 24.930068, longitude: 121.171491)
-//        objectAnnotation.coordinate = CLLocation(latitude: 24.930068, longitude: 121.171491).coordinate
-        
+        // 放上大頭針
         var objectAnnotation = MKPointAnnotation()
         objectAnnotation.coordinate = locations[0].coordinate
         objectAnnotation.title = "目前位置"
         objectAnnotation.subtitle = "現在"
-        mMapView.addAnnotation(objectAnnotation)
-        
         mMapView.tintColor = .brown
-        
+        mMapView.addAnnotation(objectAnnotation)
+
         for index in restData {
-            
             objectAnnotation = MKPointAnnotation()
             objectAnnotation.coordinate = CLLocation(latitude: index.py, longitude: index.px).coordinate
             objectAnnotation.title = index.name
             objectAnnotation.subtitle = index.add
+            mMapView.tintColor = .yellow
             mMapView.addAnnotation(objectAnnotation)
-            
         }
-
-       
+        
         mMapView.setRegion(appearRegion, animated: true)
         
     }
